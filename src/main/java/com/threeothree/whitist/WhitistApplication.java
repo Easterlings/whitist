5795f98a31fbac6794307970c7470039d38532b4
unknown <1299171412@qq.com>
2021-03-27 19:02:48 +0800

    first commit

---
+++ src/main/java/com/threeothree/whitist/WhitistApplication.java
@@ -0,0 +1,13 @@
+package com.threeothree.whitist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhitistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhitistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/threeothree/whitist/WhitistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threeothree.whitist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhitistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
